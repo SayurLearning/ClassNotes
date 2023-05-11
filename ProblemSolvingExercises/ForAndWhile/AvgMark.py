2d06bb24f6c6af6e54967a1ff35d14264f6f07c0
sayurlearning <sayurlearning@gmail.com>
2023-05-11 08:32:48 -0700

    For loop

---
+++ ProblemSolvingExercises/ForAndWhile/AvgMark.py
@@ -0,0 +1,22 @@
+#write code for both for and while loop
+#Get marks from 5  students and calculate avg
+#
+
+#for loop
+total = 0
+for i in range( #FillinMissingCode)
+    #get user input
+     #FillinMissingCode
+     total += mark
+print ("Avg is ",  #FillinMissingCode)
+
+#using while loop
+total = 0
+noOfStudents = 5
+i = 0
+while (i < noOfStudents)
+   #get user input
+     #FillinMissingCode
+     total += mark
+     i += 1
+print ("Avg is ",  #FillinMissingCode)
---
+++ ProblemSolvingExercises/ForAndWhile/BuyChoc.py
@@ -0,0 +1,23 @@
+#buy choc and cake
+# one choc is Rs 200
+# one cake is 150
+#get budget from user.
+
+#find how many choc and cake the user can buy
+
+noOfCake = 0
+noOfChoc = 0
+#get budget
+#FillinMissingCode
+
+while (money >= 150) : 
+    if (money > 200) :
+        #buy choc
+        noOfChoc += 1
+        money -= 200
+    
+    #FillinMissingCode for buying cake
+
+
+#print no of cakes and choc
+#FillinMissingCode
