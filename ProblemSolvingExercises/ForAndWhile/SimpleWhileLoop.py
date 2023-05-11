1977fb14aa02f9263f750a7aa2e134a7d01504cb
sayurlearning <sayurlearning@gmail.com>
2023-05-11 13:20:22 -0700

    while code

---
+++ ProblemSolvingExercises/ForAndWhile/SimpleWhileLoop.py
@@ -0,0 +1,26 @@
+######## Problem 1 ###############
+#Write a program that prints out a diamond shape using $.
+#After printing each line, wait for user input. If the user presses space, continue
+#If the users presses any other key, stop printing. Maximum 10 lines
+
+######## Problem 2 ###############
+# Computer will guess a random number. 
+# user has to guess that number. for each guess, print 'High' or 'Low'
+# eg - computer number - 189
+# user guess 56 - print low
+# user guess 678 - print high
+# Get user input and print 'high' or 'low' until the user guesses the number correctly 
+# https://www.w3schools.com/python/ref_random_randint.asp - read this to learn how to create a random number
+
+import #FillinMissingCode
+computerNo = random.randint(3, 9)
+
+attempt = 0
+while(True):
+    userNo = int (#FillinMissingCode)
+    #FillinMissingCode
+    attempt +=1
+
+print ("User guessed the number in ", attempt, "attempts")
+
+
