e928848d00a289173f6cb57cc1d03dd29d6377f1
sayurlearning <sayurlearning@gmail.com>
2023-05-12 15:45:03 -0700

    IfThenelse

---
+++ ProblemSolvingExercises/LogicalConditions/IfThenElse1.py
@@ -0,0 +1,42 @@
+############# Problem 1 ###########
+#Google and find the tax slabs for income tax for India. Write a program to calculate the income tax for an 
+#individual. Input is Salary. Optional input - any deductions.
+
+############ Problem 2 ####
+#Calculate the BMI and if the person is underweihgt/normal/overweight/obese
+#Google how to calculate BMI and decide on the input.
+
+########## Problem 3 ##########
+##buy choc and cake
+# one choc is Rs 200
+# one cake is 150
+#get budget from user. Also get the total number of choc and cakes that the shop has
+#Hint - you can buy choc/Cake only if shop has it.
+
+#find how many choc and cake the user can buy.
+
+############## Problem 4 ##############
+#Calculate the Grade for a student, using 3 marks.
+# The student has 100 marks in any one subject, Grade is A.
+# if the student has 90 or above in any one subject  Grade is B
+# if the student has 60 or above in any one subject  Grade is C
+# if the student has 50 or less  in all subjects  Grade is F, otherwise Grade is D.
+
+#Code
+mark1 = #FillinMissingCode
+mark2 = #FillinMissingCode
+mark3 = #FillinMissingCode
+
+if(mark1 == 100 or mark2 == 100 or mark3 == 100) #at leasr one mark is 100
+    print (#FillinMissingCode)
+elsif #FillinMissingCode 
+    print ("Grade B")
+elsif #FillinMissingCode
+    print("Garde C")
+elsif #FillinMissingCode
+    print (#FillinMissingCode)
+else 
+    print ("Grade D")
+
+
+
