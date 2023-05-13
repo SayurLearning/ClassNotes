ca28ada9b99106aa83bafda1e33c88853b076b84
sayurlearning <sayurlearning@gmail.com>
2023-05-12 20:53:59 -0700

    nestedloop

---
+++ ProblemSolvingExercises/ForAndWhile/NestedLoops.py
@@ -0,0 +1,13 @@
+######## Problem   ###############
+# Get and print the 2 marks for 3 students. Also, get each studen't name
+# output should be
+# Mark 1 for Student 1 is 55
+# Mark 2 for Student 1 is 67
+# Mark 1 for Student 2 is 56 
+#etc
+
+for student in range (#FillinMissingCode):
+    for mark in range (#FillinMissingCode):
+        inputMark = float (input(f"#FillinMissingCode")) #use formatted string
+        print (#FillinMissingCode)
+
