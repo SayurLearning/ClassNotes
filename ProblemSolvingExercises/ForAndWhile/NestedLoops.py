9c3ae48720483eb6533cfaa66689665732990133
sayurlearning <sayurlearning@gmail.com>
2023-05-13 17:53:13 -0700

    more problems

--- ProblemSolvingExercises/ForAndWhile/NestedLoops.py
+++ ProblemSolvingExercises/ForAndWhile/NestedLoops.py
@@ -1,4 +1,4 @@
-######## Problem   ###############
+######## Problem  1 ###############
 # Get and print the 2 marks for 3 students. Also, get each studen't name
 # output should be
 # Mark 1 for Student 1 is 55
@@ -10,4 +10,27 @@ for student in range (#FillinMissingCode):
     for mark in range (#FillinMissingCode):
         inputMark = float (input(f"#FillinMissingCode")) #use formatted string
         print (#FillinMissingCode)
+
+######## Problem  2 ###############
+#Print multiplication tables from 7 to 16, each number upto 12 rows.
+
+
+######## Problem  3 ###############
+#Print Chess board (alternate black and white squares)
+#print('\u25A0') - will print white Square. You can use "B" to print black squares
+
+
+######## Problem  4  ###############
+
+#print a multiplication table like below.
+#get user intput for start and end numbers. 
+#the following example uses 1 to 5.
+
+     1  2  3  4  5
+  ********************
+1 |  1  2  3  4  5
+2 |  2  4  6  8 10
+3 |  3  6  9 12 15
+4 |  4  8 12 16 20
+5 |  5 10 15 20 25
 
