cc13ee21637259da14a21c9384c51538b3deee18
sayurlearning <sayurlearning@gmail.com>
2023-05-23 21:09:34 -0700

    new file

---
+++ ProgrammingExercises/ForAndWhile/Patterns.py
@@ -0,0 +1,15 @@
+#Write code for drawing these patterns.
+#Get width and length from the user.
+# draw a empty rectangle woth char *
+#eg 
+'''
+Row 5, col 7
+* * * * * * *
+*           *
+*           *
+*           *
+* * * * * * *
+
+'''
+# next pattern is - empty diamond
+ 
