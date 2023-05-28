9b5c289fd32d73f65228cb8488e0772b71f357a7
sayurlearning <sayurlearning@gmail.com>
2023-05-27 22:20:59 -0700

    New program

---
+++ ProgrammingExercises/ForAndWhile/NestedLoops2.py
@@ -0,0 +1,56 @@
+'''
+Program 1 - Write a program to print the following pattern.
+Input is 5 for the following pattern.
+5 5 5 5 5 5 5 5 5
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+'''
+
+'''
+Get the list of employee names from the user.
+Get the monthly phone sales for each employee for the first 4 months of the year.
+Sort the employees by name in alphabetical order and for each employee sort their phones sales in 
+ascending order.
+
+Eg - Sam, Adam, Sara
+Sam's sales - 300, 567,234,1000
+Adam's Sales - 340, 456,456,234
+Sara' Sales - 1000,234,3000,2000
+
+Output sample
+Adam - 456,456,340,234
+Sam - ...
+Sara - ...
+
+'''
+
+'''
+Same details as above.
+But the input is in a file in the following format
+
+Name/Month  Sam    Adam     Sara
+Month1      300     340     1000
+Month2      567     456     234
+Month3      234     456     3000
+Month4      1000    234     2000   
+
+Find the name of the employee who had the most the phone sales each month and add it to the end of the
+table and write it back to the file
+
+eg
+Name/Month  Sam    Adam     Sara    MostSales
+Month1      300     340     1000    Sara    
+Month2      567     456     234     Sam    
+Month3      234     456     3000    Sara
+Month4      1000    234     2000    Sara
+
+
+Also , print the employee who sold most phones in all 4 months added.
+
+'''
