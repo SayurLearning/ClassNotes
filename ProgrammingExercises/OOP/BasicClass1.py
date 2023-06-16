926f974ea1686de35ea557290c107e1442035dcd
sayurlearning <sayurlearning@gmail.com>
2023-06-16 08:27:26 -0700

    one more problem

--- ProgrammingExercises/OOP/BasicClass1.py
+++ ProgrammingExercises/OOP/BasicClass1.py
@@ -19,5 +19,35 @@ maxRooms = 0
 for eachinstance in listOfInstance
     if(eachInstance.rooms) > maxRooms :
         #complete code
-        
+
      
+'''
+Define a class for an employee.
+Add a method to calculate the salary of the employee in that class based on the number of hrs worked.
+
+In the main program, you are starting a new companyy. Hiring a few employees. Each one has different per hr
+salary.
+
+Get input from the owner of the company on how many hrs each employee worked.
+Display the details of the employee who has the highest salary
+'''
+
+#define the employee class.
+#need to have a method CalculateSalary(noOfHrsWorked)
+
+#main application
+
+noOfEmployees = #get input from owner
+#For each employee, get details from owner and store it in a list
+empList.Add(newEmployee)
+#For each employee, ask the no of hrs worked for the month of May.
+maxSalary = 0
+for employee in empList
+    noOfHrsWorkedInMay = #getInput
+    #Calculate Salary
+    salary = employee.CalculateSalary(noOfHrsWorkedInMay)
+    if salary > maxSalary:
+        maxSalary = salary;
+        empWithMaxSalary = employee
+
+#print employeeDetails
