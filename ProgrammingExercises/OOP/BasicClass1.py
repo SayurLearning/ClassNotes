d513cfa9017b32bf57601801932f30c072e7adf3
sayurlearning <sayurlearning@gmail.com>
2023-06-16 07:26:26 -0700

    Class example

---
+++ ProgrammingExercises/OOP/BasicClass1.py
@@ -0,0 +1,23 @@
+'''
+Write a program for a simple hotel management (no DB)
+ Create a class for a hotel
+ Ask the user for input and create instances
+ Print which hotel has most rooms and which hotel has ratings
+
+'''
+#define your class structure
+
+noOfhotels = #Get user input.
+for i in range(noOfhotels):
+    #create a new instance of your class
+    #get user input for each of your class properties
+    #assign the input to your instance
+    #store the instance in a list
+
+#Go thru the list and find the hotel with most rooms.
+maxRooms = 0
+for eachinstance in listOfInstance
+    if(eachInstance.rooms) > maxRooms :
+        #complete code
+        
+     
