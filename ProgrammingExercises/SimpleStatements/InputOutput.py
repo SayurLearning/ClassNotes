9d8871bd93a43097e7a93e5b4167a755c851b507
sayurlearning <sayurlearning@gmail.com>
2023-06-25 14:40:10 -0700

    Inputoutput

---
+++ ProgrammingExercises/SimpleStatements/InputOutput.py
@@ -0,0 +1,18 @@
+'''
+Write code for the following program
+1. Calcualte and print users age - Write a program to ask the user for his/her name and print 'Hello', user's name. 
+Ask what year they were born.  get the year from the user. Print 'You are <age> years old'.
+
+
+2. The user just bought a few things in your  shop. Ask the user what he bought. 
+Cost of one vadai is Rs 30, one soda is Rs20 and one sandwich is Rs 100. Calculate the total cost.
+Ask the user to pay the amount. Receive the amount from the user (get money as input). 
+Print the change amount you have to give to the user. 
+Hint - use float datatype
+
+3. Write a program that will swap two numbers. 
+
+4. Ask the user to input 3 numbers. Ask the user if they want to find the max of these numbers or mininum.
+Find the answer and print.
+
+'''
